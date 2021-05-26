9bdef4429900b789b47ff7d651f4203e032885f7
Adean <adean@anymindgroup.com>
2021-05-26 13:16:36 +0700

    C61849518: setup project

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+    },
+    extends: [
+      'plugin:vue/vue3-recommended',
+      'eslint:recommended',
+      'prettier',
+    ],
+    plugins: ['vue'],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'indent': ['error', 4],
+        'quotes': ['error', 'single'],
+        'vue/html-indent': ['error', 4],
+        'no-unused-vars': 'warn'
+    }
+  }
+  
